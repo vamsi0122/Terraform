6d1bdabc48e898be5817f75b5aab3d740e56010e
vamsi0122 <suryavamsi766@gmail.com>
2025-02-15 20:19:32 +0530

    jenkins

--- terraform_vpc_module/main.tf
+++ terraform_vpc_module/main.tf
@@ -93,7 +93,7 @@ resource "aws_route_table" "private" {
   )
 }
 
-resource "aws_route_table_association" "a" {
+resource "aws_route_table_association" "private" {
   count = length(var.private_cidr)
   subnet_id      = element(aws_subnet.private[*].id, count.index)
   route_table_id = aws_route_table.private.id
