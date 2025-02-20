6faa9d9bf2be2ce266c687d779285bd78613c4c5
vamsi0122 <suryavamsi766@gmail.com>
2025-02-21 00:15:51 +0530

    all

---
+++ roboshop-infra-dev/01-vpc/main.tf
@@ -0,0 +1,17 @@
+module "vpc" {
+  source = "../../terraform_vpc_advance"
+  env = var.env
+  cidr_block = var.cidr_block
+  project_name = var.project_name
+  env = var.env
+  common_tags = var.common_tags
+  public_subnet_cidr = var.public_subnet_cidr
+  private_subnet_cidr = var.private_subnet_cidr
+  database_subnet_cidr = var.database_subnet_cidr
+
+  ###peering
+  is_peering_required = true
+  requestor_vpc_id = data.aws_vpc.default.id
+  default_route_table_id =data.aws_vpc.default.main_route_table_id
+  default_vpc_cidr = data.aws_vpc.default.cidr_block
+}
---
+++ roboshop-infra-dev/01-vpc/variables.tf
@@ -0,0 +1,53 @@
+variable "cidr_block" {
+  default = "10.0.0.0/16"
+}
+
+variable "env" {
+  default = "dev"
+}
+
+variable "project_name" {
+  default = "roboshop"
+}
+
+
+variable "common_tags" {
+  default = {
+    Project = "RoboShop"
+    Environment = "Dev"
+    Terrform = "true"
+    component = "vpc"
+  }
+}
+
+variable "vpc_tags" {
+    default = {
+        Name = "roboshop"
+    }
+  
+}
+variable "internet_gateway_tags" {
+  default = {
+    
+    Name = "internet_gateway"
+  }
+}
+
+variable "public_subnet_cidr" {
+  type = list
+  default = ["10.0.1.0/24","10.0.2.0/24"]
+}
+
+variable "private_subnet_cidr" {
+  type = list
+  default = ["10.0.11.0/24","10.0.12.0/24"]
+}
+
+variable "database_subnet_cidr" {
+  type = list
+  default = ["10.0.21.0/24","10.0.22.0/24"]
+}
+
+
+
+
